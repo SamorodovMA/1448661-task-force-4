c7c6617e136d2de8e789a76b7058864ea53059d6
Mikhail Samorodov <masamorodov@gmail.com>
2022-09-25 13:59:03 +0300

    Добавляет правки

--- controllers/TasksController.php
+++ controllers/TasksController.php
@@ -20,27 +20,28 @@ class TasksController extends Controller
             ->with('category')
             ->with('city');
 
-        if ($taskFilterForm->categories) {
-            $tasksQuery->where(['in', 'category_id', $taskFilterForm->categories]);
-        }
-        if ($taskFilterForm->remoteWork) {
-            $tasksQuery->where(['is_remote' => $taskFilterForm->remoteWork]);
-        }
-        if ($taskFilterForm->withoutResponses){
-
-            $tasksQuery->leftJoin('response', 'tasks.id = response.task_id')
-                ->where(['is', 'task_id', null]);
-        }
-        switch ($taskFilterForm->period){
-        case 'hour':
-                 $tasksQuery->where(['>', 'date_creation', new Expression('CURRENT_TIMESTAMP() - INTERVAL 1 HOUR')]);
-                break;
-            case 'day':
-                 $tasksQuery->where(['>', 'date_creation', new Expression('CURRENT_TIMESTAMP() - INTERVAL 1 DAY')]);
-                break;
-            case 'week':
-                 $tasksQuery->where(['>', 'date_creation', new Expression('CURRENT_TIMESTAMP() - INTERVAL 7 DAY')]);
-                break;
+        if ($taskFilterForm->load(\Yii::$app->request->post())) {
+            if ($taskFilterForm->categories) {
+                $tasksQuery->where(['in', 'category_id', $taskFilterForm->categories]);
+            }
+            if ($taskFilterForm->remoteWork) {
+                $tasksQuery->where(['is_remote' => $taskFilterForm->remoteWork]);
+            }
+            if ($taskFilterForm->withoutResponses) {
+                $tasksQuery->leftJoin('response', 'tasks.id = response.task_id')
+                    ->where(['is', 'task_id', null]);
+            }
+            switch ($taskFilterForm->period) {
+                case 'hour':
+                    $tasksQuery->where(['>', 'date_creation', new Expression('CURRENT_TIMESTAMP() - INTERVAL 1 HOUR')]);
+                    break;
+                case 'day':
+                    $tasksQuery->where(['>', 'date_creation', new Expression('CURRENT_TIMESTAMP() - INTERVAL 1 DAY')]);
+                    break;
+                case 'week':
+                    $tasksQuery->where(['>', 'date_creation', new Expression('CURRENT_TIMESTAMP() - INTERVAL 7 DAY')]);
+                    break;
+            }
         }
 
         $dataProvider = new ActiveDataProvider([
@@ -60,9 +61,7 @@ class TasksController extends Controller
         );
     }
 
-    /**
-     * @throws NotFoundHttpException
-     */
+
     public function actionView($id=null) {
 
         $taskIdQuery = Task::findOne($id);
