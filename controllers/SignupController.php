02acd565dadb7371f03e8c102176d64cf0323c99
Mikhail Samorodov <masamorodov@gmail.com>
2022-09-29 11:59:50 +0300

    Добавляет правки по комментариям наставника

--- controllers/SignupController.php
+++ controllers/SignupController.php
@@ -5,18 +5,36 @@ namespace app\controllers;
 use app\models\City;
 use app\models\User;
 use Yii;
+use yii\filters\AccessControl;
+use yii\helpers\ArrayHelper;
 use yii\web\Controller;
 
 class SignupController extends Controller
 {
+    public function behaviors()
+    {
+        return [
+            'access' => [
+                'class' => AccessControl::class,
+                'only' => ['index'],
+                'rules' => [
+                    [
+                        'allow' => true,
+                        'actions' => ['index'],
+                        'roles' => ['?'],
+                    ],
+                ],
+            ],
+        ];
+    }
 
     public function actionIndex()
     {
         $cities = City::find()
+            ->asArray()
             ->all();
-        foreach ($cities as $city) {
-            $citiesName[] = $city->name;
-        }
+        $cityNames = ArrayHelper::map($cities, 'id', 'name');
+
 
 
         $user = new User();
@@ -26,11 +44,11 @@ class SignupController extends Controller
             if ($user->validate()) {
                 $user->password = Yii::$app->security->generatePasswordHash($user->password);
 
-                $user->save(false);
+                $user->save();
                 $this->goHome();
             }
         }
 
-        return $this->render('index', ['model' => $user, 'cities' => $citiesName]);
+        return $this->render('index', ['model' => $user, 'cities' => $cityNames]);
     }
 }
